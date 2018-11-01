43a478e6c446c2f244601dae07bd8097f8082942
Jared Royer <jarroyer2@gmail.com>
2018-11-01 17:00:15 -0500

    switching to python

--- docker/vin-network/clean.sh
+++ docker/vin-network/clean.sh
@@ -1,6 +1,24 @@
+function removeUnwantedImages() {
+  DOCKER_IMAGE_IDS=$(docker images | awk '($1 ~ /dev-peer.*.vin_chaincode.*/) {print $3}')
+  if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
+    echo "---- No images available for deletion ----"
+  else
+    docker rmi -f $DOCKER_IMAGE_IDS
+  fi
+}
+
 rm -rf channel-artifacts
 rm -rf crypto-config
+
+export COMPOSE_PROJECT_NAME=vin_main_network
+export IMAGE_TAG=1.3.0
+
+docker-compose -f docker-compose-cli.yaml down --volumes --remove-orphans
+
+removeUnwantedImages
+
 docker kill $(docker ps -aq)
 docker rm $(docker ps -aq)
 
 docker network prune -f
+
