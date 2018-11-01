a6965bfef57035353d7731ef82d2ec3254313710
Jared Royer <jarroyer2@gmail.com>
2018-11-01 15:40:14 -0500

    stuck with vin network using old version of chaincode

--- node/query.js
+++ node/query.js
@@ -54,8 +54,8 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 	const request = {
 		//targets : --- letting this default to the peers assigned to the channel
 		chaincodeId: 'vin_chaincode',
-		fcn: 'getCar'
-		args: ['abc123']
+		fcn: 'getUser',
+		args: ['DMV']
 	};
 
 	// send the query proposal to the peer
